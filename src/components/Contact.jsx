0ade9c5cfab7c9d3af906bb670fb0e7cf4428590
Kushals-Air <kushalchoudhary3121@gmail.com>
2024-11-18 18:37:24 -0600

    implement the contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_6gbs1be',
+        'template_ir29emg',
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'Kushal',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'kushalchoudhary3121@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '0tw9VvWFo_08VO6B9'
       )
       .then(
         () => {
